99dc583917d9ec39e700cc12d1c6c36a38ab5941
MrHuman0 <olexandrstehaylo@gmail.com>
2022-09-28 12:55:24 +0300

    initial commit

---
+++ src/Contexts/AuthContext.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from "react";
+import {useNavigate} from 'react-router-dom'
+import {app} from "../Components/FireBase";
+import {onAuthStateChanged} from "firebase/auth";
+
+const AuthContext = React.createContext();
+
+export const AuthProvider = ({children}) => {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const history = useNavigate();
+
+    useEffect(() => {
+        onAuthStateChanged(app, (user) => {
+            setUser(user);
+            setLoading(false);
+            if (user) history('/main');
+            if (!user) history('/');
+        })
+    }, [user, history]);
+
+    const value = {user};
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
